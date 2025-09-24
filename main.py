5ab5a38b2f81f11923fa46f53517b7bcf2bca826
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-09-24 17:34:37 +0300

    user input support

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def can_vote(age, citizenship, is_disqualified):
         return True
 
 
-age = 18
-citizenship = "Россия"
-is_disqualified = False
+try:
+    age = int(input("Введите ваш возраст: "))
+    citizenship = input("Введите страну, в которой у вас гражданство: ")
+    disqualified_input = int(input("Дисквалифицированы ли вы с выборов (0 - нет, 1 - да): "))
+    assert disqualified_input == 0 or disqualified_input == 1
 
-print("Вы можете голосовать") if can_vote(age, citizenship, is_disqualified) else  print("Вы не можете голосовать")
+    is_disqualified = bool(disqualified_input)
+except:
+    print("Веденные данные - некорректны")
+else:
+    print("Вы можете голосовать") if can_vote(age, citizenship, is_disqualified) else print("Вы не можете голосовать")
